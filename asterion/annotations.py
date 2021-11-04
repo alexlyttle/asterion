60bf3a9037f77c522d54de39fee08aaed33e28b8
alexlyttle <a.j.lyttle@outlook.com>
2021-11-04 17:36:25 +0000

    Remove old annotations and add DistLike

--- asterion/annotations.py
+++ asterion/annotations.py
@@ -2,51 +2,53 @@
 """
 from __future__ import annotations
 
-import numpy as np
-from typing import Union, TypeVar, Sequence, Generic
-
+from typing import Union
+from collections.abc import Iterable
+from numpyro.distributions import Distribution
 
 __all__ = [
-    "T",
-    "Array1D",
-    "Array2D",
-    "Array3D",
+    "DistLike",
 ]
 
+# T = TypeVar('T')  # type
+# """A generic type variable
+# """
 
-T = TypeVar('T')  # type
-""".. _type-variable:
+# S = TypeVar('S')  # sequence
+# Array = Union[S, np.ndarray]
 
-A generic type variable
+
+# class Array1D(Generic[T]):
+#     """A generic type for a 1-dimensional array-like object
+    
+#     alias of Union\[Sequence\[:obj:`T`\], :obj:`numpy.ndarray`\]
+#     """
+#     def __getitem__(self, T):
+#         return Array[Sequence[T]]
+
+
+# class Array2D(Generic[T]):
+#     """A generic type for a 2-dimensional array-like object
+    
+#     alias of Union\[Sequence\[Sequence\[:obj:`T`\]\], :obj:`numpy.ndarray`\]
+#     """
+#     def __getitem__(self, T):
+#         return Array[Sequence[Sequence[T]]]
+
+
+# class Array3D(Generic[T]):
+#     """A generic type for a 3-dimensional array-like object
+    
+#     alias of Union\[Sequence\[Sequence\[Sequence\[:obj:`T`\]\]\],
+#     :obj:`numpy.ndarray`\]
+#     """
+#     def __getitem__(self, T):
+#         return Array[Sequence[Sequence[Sequence[T]]]]
+
+
+DistLike = Union[Iterable, Distribution]  
+"""A generic type for a distribution object which is accepted as the argument
+of :func:`distribution`.
 """
 
-S = TypeVar('S')  # sequence
-Array = Union[S, np.ndarray]
-
-
-class Array1D(Generic[T]):
-    """A generic type for a 1-dimensional array-like object
-    
-    alias of Union\[Sequence\[:obj:`T`\], :obj:`numpy.ndarray`\]
-    """
-    def __getitem__(self, T):
-        return Array[Sequence[T]]
-
-
-class Array2D(Generic[T]):
-    """A generic type for a 2-dimensional array-like object
-    
-    alias of Union\[Sequence\[Sequence\[:obj:`T`\]\], :obj:`numpy.ndarray`\]
-    """
-    def __getitem__(self, T):
-        return Array[Sequence[Sequence[T]]]
-
-
-class Array3D(Generic[T]):
-    """A generic type for a 3-dimensional array-like object
-    
-    alias of Union\[Sequence\[Sequence\[Sequence\[:obj:`T`\]\]\],
-    :obj:`numpy.ndarray`\]
-    """
-    def __getitem__(self, T):
-        return Array[Sequence[Sequence[Sequence[T]]]]
+# alias of Union\[:obj:`Iterable`, :obj:`Distribution`\]
