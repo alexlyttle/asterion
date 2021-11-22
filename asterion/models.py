632ba6fec2e4e5d7eda03a23079f24c55638c0c6
alexlyttle <a.j.lyttle@outlook.com>
2021-11-22 16:42:13 +0000

    Change to optional

--- asterion/models.py
+++ asterion/models.py
@@ -44,7 +44,7 @@ __all__ = [
 
 def distribution(
     value: DistLike,
-    default_dist: dist.Distribution=dist.Normal,
+    default_dist: Optional[dist.Distribution]=None,
 ) -> dist.Distribution:
     """Return a numpyro distribition.
 
@@ -52,13 +52,16 @@ def distribution(
     unpacking value as its arguments.
 
     Args:
-        value (:term:`dist_like`): Iterable of args to pass to default_dist, or a Distribution.
-        default_dist (numpyro.distributions.distribution.Distribution, optional):
-            [description]. Defaults to dist.Normal.
+        value (:term:`dist_like`): Iterable of args to pass to default_dist, 
+            or a Distribution.
+        default_dist (numpyro.distributions.distribution.Distribution, \
+optional): Default distribution. Defaults to dist.Normal if None.
 
     Returns:
         numpyro.distributions.distribution.Distribution: [description]
     """
+    if default_dist is None:
+        default_dist = dist.Normal
     if not isinstance(value, dist.Distribution):
         if not isinstance(value, Iterable):
             value = (value,)
