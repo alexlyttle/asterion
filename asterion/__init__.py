be80b171ef80db593233cb636bc7de77ab0b6ef7
Alex Lyttle <a.j.lyttle@outlook.com>
2022-02-07 11:26:37 +0000

    Add packagedir

--- asterion/__init__.py
+++ asterion/__init__.py
@@ -3,9 +3,12 @@
 Warning:
     This module is a work-in-progress, use with caution.
 """
+import os
 import numpyro
 
 from ._version import __version__
 
 numpyro.enable_x64()
 numpyro.set_host_device_count(10)
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
