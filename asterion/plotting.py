c2acb50a399f3f1a22d62011329967f21c3baf22
Alex Lyttle <a.j.lyttle@outlook.com>
2022-02-22 17:13:29 +0000

    Add echelle plot

--- asterion/plotting.py
+++ asterion/plotting.py
@@ -10,9 +10,43 @@ from matplotlib.ticker import MaxNLocator
 from matplotlib.figure import Figure
 from corner import corner
 from arviz.labels import MapLabeller
-from typing import List, Optional, Dict, Union
+from typing import List, Optional, Union
 
-from .annotations import DistLike
+__all__ = [
+    'plot_glitch',
+    'plot_corner',
+    'get_labeller',
+]
+
+def _validate_predictive_group(data: az.InferenceData, group: str):
+    """Validate the predictive groups in data.
+
+    Args:
+        data (arviz.InferenceData): Inference data object.
+        group (str): One of ['posterior', 'prior'].
+
+    Raises:
+        ValueError: If group is not valid.
+        KeyError: If predictive is not in data, gives helpful suggestion.
+
+    Returns:
+        xarray.Dataset: Dataset corresponding to the predictive of group.
+    """
+    if group == 'posterior':
+        key = 'posterior_predictive'
+        predictive = data.get(key, None)
+    elif group == 'prior':
+        key = 'prior_predictive'
+        predictive = data.get(key, None)
+    else:
+        raise ValueError(
+            f"Group '{group}' is not one of ['posterior', 'prior']."
+        )
+    
+    if predictive is None:
+        raise KeyError(f"Group '{key}' not in data. Consider using method " +
+                       "'Inference.{key}()' to sample the predictive.")
+    return predictive
 
 def plot_glitch(data: az.InferenceData, group='posterior', kind: str='He',
                 quantiles: Optional[List[float]]=None, 
@@ -32,12 +66,7 @@ def plot_glitch(data: az.InferenceData, group='posterior', kind: str='He',
     Returns:
         matplotlib.axes.Axes: Axis on which the glitch is plot.
     """
-    if group == 'posterior':
-        predictive = data.posterior_predictive
-    elif group == 'prior':
-        predictive = data.prior_predictive
-    else:
-        raise ValueError(f"Group '{group}' is not one of ['posterior', 'prior'].")
+    predictive = _validate_predictive_group(data, group)
 
     if quantiles is None:
         quantiles = [.16, .84]
@@ -92,6 +121,91 @@ def plot_glitch(data: az.InferenceData, group='posterior', kind: str='He',
     ax.legend()
     return ax
 
+def plot_echelle(data: az.InferenceData, group='posterior',
+                 kind: str='full', delta_nu: Optional[float]=None,
+                 quantiles: Optional[List[float]]=None, 
+                 ax: plt.Axes=None) -> plt.Axes:
+    """Plot an echelle diagram of the data.
+    
+    Choose to plot the full mode, background model or glitchless model. This is
+    compatible with data from inference on models like :class:`GlitchModel`.
+
+    Args:
+        data (az.InferenceData): Inference data object.
+        group (str): On of ['posterior', 'prior']. Defaults to 'posterior'.
+        kind (str): One of ['full', 'glitchless', 'background']. Defaults to 
+            'full' which plots the full model for nu. Use 'glitchless' to plot
+            the model without the glitch components. Use 'background' to plot
+            the background component of the model.
+        delta_nu (float, optional): _description_. Defaults to None.
+        quantiles (iterable, optional): Quantiles to plot as confidence
+            intervals. If None, defaults to the 68% confidence interval. Pass
+            an empty list to plot no confidence intervals.
+        ax (matplotlib.axes.Axes): Axis on which to plot the echelle.
+
+    Returns:
+        matplotlib.axes.Axes: Axis on which the echelle is plot.
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    if quantiles is None:
+        quantiles = [.16, .84]
+
+    predictive = _validate_predictive_group(data, group)
+    dim = ('chain', 'draw')  # dim over which to take stats
+
+    if delta_nu is None:
+        delta_nu = data[group]['delta_nu'].median(dim=dim).to_numpy()
+    
+    nu = data.observed_data.nu
+    nu_err = data.constant_data.nu_err
+    n_pred = predictive.n_pred
+
+    if group != 'prior':
+        # Plot observed - prior predictive should be independent of obs
+        ax.errorbar(nu%delta_nu, nu,
+                    xerr=nu_err, color='C0', marker='o',
+                    linestyle='none', label='observed')
+    
+    if kind == 'full':
+        y = predictive['nu_pred']
+    elif kind == 'background':
+        y = predictive['nu_bkg_pred']
+    elif kind == 'glitchless':
+        y = predictive['nu_pred'] - predictive.get('dnu_he_pred', 0.0) - \
+            predictive.get('dnu_cz_pred', 0.0)
+        y.attrs['unit'] = predictive['nu_pred'].attrs['unit']
+
+    y_mod = (y - n_pred*delta_nu)%delta_nu
+    y_med = y.median(dim=dim)
+    ax.plot(y_mod.median(dim=dim), y_med, label=kind, color='C1')
+
+    y_mod_quant = y_mod.quantile(quantiles, dim=dim)
+    num_quant = len(quantiles)//2
+    alphas = np.linspace(0.1, 0.5, num_quant*2+1)
+    for i in range(num_quant):
+        delta = quantiles[-i-1] - quantiles[i]
+        ax.fill_betweenx(y_med, y_mod_quant[i], y_mod_quant[-i-1],
+                        color='C1', alpha=alphas[2*i+1],
+                        label=f'{delta:.1%} CI')
+
+    xlabel = [r'$\nu\,\mathrm{mod}\,{' + f'{delta_nu:.2f}' + '}$']
+    unit = u.Unit(y.attrs.get('unit', ''))
+    if str(unit) != '':
+        xlabel.append(unit.to_string(format='latex_inline'))
+    ax.set_xlabel('/'.join(xlabel))
+    
+    ylabel = [r'$\nu$']
+    unit = u.Unit(nu.attrs.get('unit', ''))
+    if str(unit) != '':
+        ylabel.append(unit.to_string(format='latex_inline'))
+    ax.set_ylabel('/'.join(ylabel))
+    
+    ax.legend()
+
+    return ax
+    
 def get_labeller(data: az.InferenceData, group: str='posterior', 
                  var_names: Optional[List[str]]=None) -> MapLabeller:
     """Get labeller for use with arviz plotting. This automatically searches
@@ -100,12 +214,12 @@ def get_labeller(data: az.InferenceData, group: str='posterior',
     
     Args:
         data (arviz.InferenceData): Inference data object.
-        group (str, optional): Inference data group for which to map labels.
-        var_names (list of str, optional): Variable names for which to map
-            labels.
+        group (str): Inference data group for which to map labels.
+        var_names (list[str], optional): Variable names for
+            which to map labels.
         
     Returns:
-        arviz.labels.MapLabeller: Label map
+        arviz.labels.MapLabeller: Label map.
     """
     if var_names is None:
         var_names = list(data[group].keys())  # Use all variables (dangerous)
@@ -144,14 +258,14 @@ def plot_corner(data: az.InferenceData, group: str='posterior',
 
     Args:
         data (arviz.InferenceData): Inference data object.
-        group (str, optional): Inference data group from which to take samples.
+        group (str): Inference data group from which to take samples.
             Defaults to 'posterior'.
-        var_names (list of str, optional): Variable names to plot. 
+        var_names (List[str], optional): Variable names to plot. 
             Defaults to plotting all available variables.
         quantiles (iterable, optional): Quantiles to plot as dashed lines in
             the marginals. If None, defaults to the 68% confidence interval. 
             Pass an empty list to plot no confidence intervals.
-        labeller (str or MapLabeller, optional): Labeller which maps variable
+        labeller (str, or MapLabeller): Labeller which maps variable
             names to their axis labels. Defaults to 'auto'.
         **kwargs: Keyword arguments to pass to :func:`corner.corner`.
 
