7ae3a44f16ecf58f657cfb65015724495d5e652b
Alex Lyttle <a.j.lyttle@outlook.com>
2022-02-22 17:11:00 +0000

    Create utils file

--- asterion/__init__.py
+++ asterion/__init__.py
@@ -1,17 +1,13 @@
-"""Fitting acoustic glitches to 
+"""Fitting acoustic glitches to the mode frequencies of solar-like oscillators.
 
 Warning:
     This module is a work-in-progress, use with caution.
 """
-import os
-
-PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 import numpyro
 numpyro.enable_x64()
 
-from ._version import __version__
-from .inference import Inference
+from .version import __version__
 from .models import GlitchModel
+from .inference import Inference
 from .plotting import plot_corner, plot_glitch, get_labeller
 from .results import get_dims, get_summary, get_table, get_var_names
---
+++ asterion/utils.py
@@ -0,0 +1,34 @@
+import os
+import numpyro.distributions as dist
+
+from typing import Optional, Iterable
+from .annotations import DistLike
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def distribution(
+    value: DistLike,
+    default_dist: Optional[dist.Distribution]=None,
+) -> dist.Distribution:
+    """Return a numpyro distribition.
+
+    If value is not a distribution, this returns the default distribution,
+    unpacking value as its arguments.
+
+    Args:
+        value (:term:`dist_like`): Iterable of args to pass to default_dist, 
+            or a Distribution.
+        default_dist (numpyro.distributions.distribution.Distribution, \
+optional): Default distribution. Defaults to dist.Normal if None.
+
+    Returns:
+        numpyro.distributions.distribution.Distribution: [description]
+    """
+    if default_dist is None:
+        default_dist = dist.Normal
+    if not isinstance(value, dist.Distribution):
+        if not isinstance(value, Iterable):
+            value = (value,)
+        value = default_dist(*value)
+    return value
+
