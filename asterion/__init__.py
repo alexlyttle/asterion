1c7093261d76ce9bf9df3634d2bded927909ef50
Alex Lyttle <a.j.lyttle@outlook.com>
2022-02-23 13:04:42 +0000

    Add plot_echelle to __init__ and example

--- asterion/__init__.py
+++ asterion/__init__.py
@@ -4,10 +4,11 @@ Warning:
     This module is a work-in-progress, use with caution.
 """
 import numpyro
+
 numpyro.enable_x64()
 
 from .version import __version__
 from .models import GlitchModel
 from .inference import Inference
-from .plotting import plot_corner, plot_glitch, get_labeller
+from .plotting import plot_corner, plot_echelle, plot_glitch, get_labeller
 from .results import get_dims, get_summary, get_table, get_var_names
